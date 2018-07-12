e44417287af90bf9e994fc28d170328e0497cb4a
raz0r <me@raz0r.name>
2018-07-12 15:22:03 +0300

    add Travis CI

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/Raz0r/aemscan.svg?branch=master)](https://travis-ci.org/Raz0r/aemscan)
+
 ## aemscan
 #### *Adobe Experience Manager Vulnerability Scanner*
 
--- aemscan/tests/test_get_version.py
+++ aemscan/tests/test_get_version.py
@@ -1,8 +1,10 @@
 __author__ = 'raz0r'
 
+import os
 from ..tasks import get_version
 
 
 def test_get_version():
-    with open('.feed.xml', 'r') as f:
+    path = os.path.dirname(os.path.abspath(__file__)) + '/'
+    with open(path + '.feed.xml', 'r') as f:
         assert get_version.parse_response(f.read()) == '6.0'
