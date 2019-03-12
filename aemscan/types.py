9173e205bba2e212f09e10440d7d7eb69cb478ac
Syed Abuthahir <abuvanth@gmail.com>
2019-03-12 12:44:45 +0530

    Update types.py

--- aemscan/types.py
+++ aemscan/types.py
@@ -1,7 +1,10 @@
 __author__ = 'raz0r'
 
 import click
-from urlparse import urlparse
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
 
 
 class URL(click.ParamType):
