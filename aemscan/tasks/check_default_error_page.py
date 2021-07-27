f6c6118c2d5e83001dc083b313c8eea8f463a20c
Binit Ghimire <thebinitghimire@gmail.com>
2021-07-27 12:20:14 +0545

    Added support for insecure server connections! (#12)

--- aemscan/tasks/check_default_error_page.py
+++ aemscan/tasks/check_default_error_page.py
@@ -3,10 +3,12 @@ __author__ = 'raz0r'
 import re
 import click
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def run(url):
-    response = requests.get(url + '/non_existing')
+    response = requests.get(url + '/non_existing', verify=False)
     banner = parse_response(response.content)
     if banner:
         click.echo(click.style('Custom error pages are not enabled, service banner: {}'.format(banner), fg='green'))
--- aemscan/tasks/check_webdav.py
+++ aemscan/tasks/check_webdav.py
@@ -2,10 +2,12 @@ __author__ = 'raz0r'
 
 import click
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def run(url):
-    response = requests.options(url + '/')
+    response = requests.options(url + '/', verify=False)
     if 'propfind' in response.headers.get('allow', '').lower() or response.status_code == 401:
         click.echo(click.style('WebDAV is enabled', fg='green'))
     else:
